649b413d16b4e7657e03d8da73f9d1df7a875a83
Andrew Gorman <andrew.gorman@verizonmedia.com>
2022-05-06 19:33:28 +0100

    add graphic controller

--- src/components/functional_controls/functional_controls.py
+++ src/components/functional_controls/functional_controls.py
@@ -1,47 +1,89 @@
 from concurrent.futures.process import _system_limits_checked
 from ..base.base import BaseComponent
-from tkinter import ttk, Label, Frame
+from tkinter import ttk, Label, Frame, Canvas
+import numpy as np
+import math
+
+center = 100, 100
 
 class FunctionalControls(BaseComponent):
-    # Slider variables, must be unique values for some reason.
-    s0, s1, s2 = 1, 99.9, 100
-    defaultVolume, defaultDelay, defaultDecay = 0.5, 0, 0
-    currentVolume, currentDelay, currentDecay = None, None, None
+    WIDTH, HEIGHT = 300, 300
+    MID_X, MID_Y = WIDTH/2, HEIGHT/2
+    MIN_ANGLE, DEFAULT_ANGLE, MAX_ANGLE = -160, 0, 160
+    CURRENT_VALUE = DEFAULT_ANGLE
+    LAST_X_MOUSE_POS, LAST_Y_MOUSE_POS = 0, 0
 
     def __init__(self, app, audioController):
         super().__init__(app)
         self.audioController = audioController
-        
-        self.frame = Frame(self.root)
-        self.frame.columnconfigure(1, weight=1, uniform='column')
-        self.frame.rowconfigure(1, weight=0, uniform='row')
+        self.canvas = Canvas(self.root, relief='flat', width=self.WIDTH, height=self.HEIGHT)
 
-        self.volume, self.currentVolume = self.renderControlWidget('Level', 0, 1, self.s0, self.defaultVolume, self.onVolumeChange)
+        self.bezel_coords = [self.MID_X, self.MID_Y, 100, 0, 0, 0, 0, 0]
+        self.bezel_id = self.create_object(self.bezel_coords, 'grey40')
 
-        self.volume.grid(column=1, row=1, padx=25, pady=5, sticky='ew')
+        self.base_coords = [self.MID_X, self.MID_Y, 85, 0, 0, 0, 0, 0]
+        self.base_id = self.create_object(self.base_coords, 'grey60')
 
-        # global layout
-        self.frame.grid(column=1, row=0, sticky='ew', rowspan=4)
+        self.knob_coords = [self.MID_X, self.MID_Y, 10, 60, self.DEFAULT_ANGLE, 0, 0, 0]
+        self.knob_id = self.create_object(self.knob_coords, 'white')
 
-    def renderControlWidget(self, label, minValue, maxValue, var, defaultValue, onChangeFunction):
-        localFrame = Frame(self.frame)
-        localFrame.columnconfigure(1, weight=1)
-        label = Label(localFrame, text=label, font=('Helvetica, 16'))
-        valueLabel = Label(localFrame, text=defaultValue)
-        slider = ttk.Scale(
-            localFrame,
-            from_=minValue,
-            to=maxValue,
-            orient='horizontal',
-            variable=var,
-            command=onChangeFunction
-        )
-        slider.set(defaultValue)
-        label.grid(column=1, row=0, sticky='w', pady=10)
-        slider.grid(column=1, row=1, sticky='ew')
-        valueLabel.grid(column=1, row=2, sticky='ne')
-        return localFrame, valueLabel
+        self.value = self.canvas.create_text(self.MID_Y, self.HEIGHT-30, font='Helvetica 12', text='50.0', fill='white')
+
+        self.canvas.bind("<B1-Motion>", self.moveKnob)
+        self.canvas.grid(column=1, row=0, sticky='ew', rowspan=4)
+
+        self.volume = 50
+
+    def create_object(self, data, color):
+        x1, y1, x2, y2 = self.calculate_position(data)
+        return self.canvas.create_oval(x1, y1, x2, y2, fill=color, outline='grey50')
+
+    def moveKnob(self, event):
+        x, y = event.x, event.y
+        # Determine whether user is increasing/decreasing knob value
+        if y < self.LAST_Y_MOUSE_POS and self.CURRENT_VALUE < self.MAX_ANGLE:
+            self.CURRENT_VALUE += 2
+        if y > self.LAST_Y_MOUSE_POS and self.CURRENT_VALUE > self.MIN_ANGLE:
+            self.CURRENT_VALUE -= 2
+
+        # set new knob angle
+        self.knob_coords[4] = self.CURRENT_VALUE
+        self.move_object(self.knob_id, self.knob_coords)
+        self.LAST_X_MOUSE_POS, self.LAST_Y_MOUSE_POS = x, y
+
+        # Calculate volume based on current knob position
+        scaler = 100 / (abs(self.MIN_ANGLE) + abs(self.MAX_ANGLE))
+        self.volume = (self.CURRENT_VALUE * scaler) + 50
+        self.canvas.itemconfig(self.value, text=self.volume)
     
+    def calculate_position(self, data):
+        # Largely taken from - https://stackoverflow.com/questions/41451690/how-to-make-tkinter-object-move-in-circlular-path
+        center_x, center_y, radius, distance, angle, angle_speed, x, y = data
+
+        # calculate new position of object
+        x = center_x - distance * math.sin(math.radians(-angle))
+        y = center_y - distance * math.cos(math.radians(-angle))
+
+        # save positon so other object can use it as its center of rotation
+        data[5] = x
+        data[6] = y
+
+        # calcuate oval coordinates
+        x1 = x - radius
+        y1 = y - radius
+        x2 = x + radius
+        y2 = y + radius
+
+        return x1, y1, x2, y2
+        
+    def move_object(self, object_id, data):
+        # Largely taken from - https://stackoverflow.com/questions/41451690/how-to-make-tkinter-object-move-in-circlular-path
+        # calculate oval coordinates
+        x1, y1, x2, y2 = self.calculate_position(data)
+
+        # move oval
+        self.canvas.coords(object_id, x1, y1, x2, y2)
+
     def onVolumeChange(self, event):
         if not self.currentVolume == None:
             self.currentVolume.configure(text=event[0:4])
